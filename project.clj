b24f437a6592d56f264474f6b65bad034a0f911e
Milinda Pathirage <milinda.pathirage@gmail.com>
2014-09-26 10:23:16 -0400

    updated storm dependency version

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :source-paths      ["src/clojure"]
   :java-source-paths ["src/java"]
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.apache.storm/storm-core "0.9.2-incubating-SNAPSHOT"]
+                 [org.apache.storm/storm-core "0.9.2-incubating"]
                  [org.clojure/tools.macro "0.1.2"]
                  [org.apache.thrift/libthrift "0.7.0" :exclusions [org.slf4j/slf4j-api]]]
   :target-path "target/%s"
